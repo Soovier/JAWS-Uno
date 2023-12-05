f31923a52b6d5c5e03a5f0a6cf8b55afa73fa2e6
Soovier <sosunkunle@gmail.com>
2023-12-05 17:48:25 -0500

    Display Cards Committ

--- src/componets/playerMain.svelte
+++ src/componets/playerMain.svelte
@@ -1,86 +1,91 @@
 <!-- MyComponent.svelte -->
 <script lang="ts">
-	import * as cards from "../lib/main";
-	import cardBack from "../lib/cardBack (1).png";
-	import cardsImgs from "$lib/files";
-	let deck = new cards.Deck(1);
-	let cardss = deck.cards;
+  import * as cards from "../lib/main";
+  import cardBack from "../lib/cardBack (1).png";
+  import cardsImgs from "$lib/files";
+  let deck = new cards.Deck(1);
+  let cardss = deck.cards;
 
-	console.log("Working");
+  console.log("Working");
 </script>
 
 <body>
-	<li class="boxplay center">
-		<div class="play p1">1</div>
-		<div class="play p2">2</div>
-		<div class="play p3">3</div>
-		<div class="play p4">4</div>
-	</li>
+  <li class="boxplay center">
+    <div class="play p1">1</div>
+    <div class="play p2">2</div>
+    <div class="play p3">3</div>
+    <div class="play p4">4</div>
+  </li>
 
-	<div style="width:200px; height:300px;">
-		{#each Object.entries(cardsImgs) as [key, value]}
-			<img class="cardImages" src={value} alt="Card image" />
-		{/each}
-	</div>
+  <div style="width:{100 * Object.keys(cardsImgs).length}px; height:300px;">
+    {#each Object.entries(cardsImgs) as [key, value], i}
+      <img
+        style="width: {100 + i * 2}px"
+        class="cardImages"
+        src={value}
+        alt="Card image"
+      />
+    {/each}
+  </div>
 
-	<div class="container">
-		<img class="backHand" src={cardBack} alt="DECK" />
-	</div>
+  <div class="container">
+    <img class="backHand" src={cardBack} alt="DECK" />
+  </div>
 </body>
 
 <style>
-	body {
-		overflow: hidden;
-		padding: 0;
-	}
+  body {
+    overflow: hidden;
+    padding: 0;
+  }
 
-	.boxplay {
-		list-style: none;
-		color: rgb(255, 0, 0);
-	}
+  .boxplay {
+    list-style: none;
+    color: rgb(255, 0, 0);
+  }
 
-	.container {
-		position: absolute;
-		left: 650px;
-		bottom: 500px;
-	}
+  .container {
+    position: absolute;
+    left: 650px;
+    bottom: 500px;
+  }
 
-	.backHand {
-		/* position: absolute; */
-		width: 120px;
-	}
+  .backHand {
+    /* position: absolute; */
+    width: 120px;
+  }
 
-	.play {
-		width: 850px;
-		height: 850px;
-		background-color: rgb(255, 132, 0);
-		position: absolute;
-	}
+  .play {
+    width: 850px;
+    height: 850px;
+    background-color: rgb(255, 132, 0);
+    position: absolute;
+  }
 
-	.p1 {
-		left: 85%;
-		bottom: 100px;
-		transform: rotate(90deg);
-	}
+  .p1 {
+    left: 85%;
+    bottom: 100px;
+    transform: rotate(90deg);
+  }
 
-	.p2 {
-		right: 85%;
-		bottom: 100px;
-	}
+  .p2 {
+    right: 85%;
+    bottom: 100px;
+  }
 
-	.p3 {
-		top: 85%;
-		bottom: 100px;
-		right: 450px;
-		width: 1000px;
-		transform: rotate(270deg);
-	}
+  .p3 {
+    top: 85%;
+    bottom: 100px;
+    right: 450px;
+    width: 1000px;
+    transform: rotate(270deg);
+  }
 
-	.p4 {
-		bottom: 85%;
-		/* bottom: 100px; */
-		right: 450px;
-		width: 1000px;
-		transform: rotate(270deg);
-	}
+  .p4 {
+    bottom: 85%;
+    /* bottom: 100px; */
+    right: 450px;
+    width: 1000px;
+    transform: rotate(270deg);
+  }
 </style>
