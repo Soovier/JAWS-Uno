3e83aacf30539f59c69c0f5aaa0911b988ee96b9
Soovier <sosunkunle@gmail.com>
2023-12-05 21:46:12 -0500

    Cards Working

--- src/componets/playerMain.svelte
+++ src/componets/playerMain.svelte
@@ -11,21 +11,25 @@
   let player2 = game.players[1];
 
   console.log(player1.cards);
-
-  console.log("Working");
 </script>
 
-<body>
-  <div class="backContain" style="width:100%; height:100% z-index: 10; ">
-    {#each player1.cards as card, i}
-      <!-- svelte-ignore a11y-img-redundant-alt -->
-      <img
-        style="width: {100 + i * 0.1}px"
-        class="cardImages"
-        src={cardsImgs[card.cardString]}
-        alt="Card image"
-      />
-    {/each}
+<body class="disBox forplayer1">
+  <div>
+    <div class="backContain" style="width:100%; height:100% z-index: 10; ">
+      {#each player1.cards as card, i}
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <!-- svelte-ignore a11y-missing-content -->
+        <a href="#">
+          <img
+            style="width: {125 + i * 0.5}px"
+            class="cardImages"
+            src={cardsImgs[card.cardString]}
+            alt="Card image"
+          />
+        </a>
+      {/each}
+    </div>
   </div>
 
   <div class="container">
@@ -36,14 +40,34 @@
 <style>
   body {
     overflow: hidden;
-    padding: 0;
+    /* padding: 0; */
+    width: 100%;
+    margin: 0 auto;
+    height: 98vh;
+    background-image: url("$lib/woodback.png");
+    background-color: rgb(255, 221, 0);
+  }
+
+  .disBox {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+  }
+
+  .cardImages {
+    transition: all 0.6s ease-in-out;
+  }
+
+  .cardImages:hover {
+    margin-bottom: 25px;
+    height: 190px;
   }
 
   .backContain {
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: red;
+    /* background-color: rgb(0, 0, 0); */
   }
 
   .container {
